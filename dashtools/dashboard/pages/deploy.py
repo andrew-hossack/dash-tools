4d1836cab29a56b926d11683236c0665859b7976
Andrew Hossack <andrew_hossack@outlook.com>
2022-10-05 17:45:55 -0500

    Added file upload inactive

--- dashtools/dashboard/pages/deploy.py
+++ dashtools/dashboard/pages/deploy.py
@@ -1,9 +1,8 @@
-from dash import html, dcc
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import visdcc
-
-
-ROW_STYLE = {'padding-left': '20px'}
+from dash import dcc, html
+from dash_iconify import DashIconify
 
 
 class Terminal:
@@ -24,15 +23,140 @@ class Terminal:
 terminal = Terminal()
 
 
+def file_explorer():
+    return html.Div([
+        dmc.Text('File Explorer'),
+        html.Div([
+            dmc.Space(h=5),
+            dcc.Upload(
+                # https://www.dash-mantine-components.com/components/button
+                dmc.Button(
+                    [
+                        DashIconify(
+                            icon='ant-design:file-add-outlined', width=30),
+                        dmc.Text('Open File', inline=True,
+                                 size='md', weight='bold')
+                    ],
+                    variant='subtle',
+                    style={'color': 'inherit', 'text-decoration': 'none'},
+                )
+            )
+
+        ], style={'width': '100%', 'height': '200px', 'text-align': 'center', 'background-color': 'red'})
+    ])
+
+
+def deploy_info():
+    return html.Div([
+        dmc.Text('Deployment Information'),
+        dmc.Space(h=5),
+        html.Div(
+            [
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="File Exists: app.py",
+                            checked=True,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="File Exists: Procfile",
+                            checked=True,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="File Exists: requirements.txt",
+                            checked=True,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="File Exists: runtime.txt",
+                            checked=False,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="server = app.server",
+                            checked=False,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+                dmc.Tooltip(
+                    label="This is a tooltip",
+                    position="left",
+                    placement="center",
+                    gutter=10,
+                    children=[
+                        dmc.Checkbox(
+                            id="checkbox",
+                            label="App Name Available",
+                            checked=False,
+                            disabled=True,
+                            color="green")
+                    ],
+                    style={'padding-bottom': '10px'}
+                ),
+            ]
+        )
+    ], style={"height": 'auto', "width": 300, "overflow": "auto"})
+
+
 def terminal_box():
     return html.Div(
         [
             visdcc.Run_js(id='deploy-terminal-runjs', run=""),
             dcc.Interval(id='deploy-terminal-refresh-interval',
                          interval=1000, n_intervals=0, disabled=False),
+            dmc.Text('Terminal'),
+            dmc.Space(h=5),
             html.Textarea(id='deploy-terminal',
                           contentEditable="false",
-                          placeholder="Terminal Output (Empty)",
                           readOnly='true',
                           draggable='false',
                           style={
@@ -51,10 +175,9 @@ def render():
 
     return html.Div(
         [
-            dbc.Row(['Row1']),
-            dbc.Row([
-                terminal_box()
-            ]),
+            dbc.Row([file_explorer()]),
+            dbc.Row([deploy_info()]),
+            dbc.Row([terminal_box()]),
         ],
         style={"background-color": "#aaaaaa",
                "height": "90vh", "padding": "10px"}
